llm-d/llm-d-inference-scheduler

Add plugin state debug endpoint
Expose a metrics-server debug handler that dumps sanitized state from plugins that opt in via a new StateDumper interface. Add a concrete in-flight load state dumper and focused unit coverage for handler behavior and deterministic output.

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>
Signed-off-by: wenhug <50309350+wenhug@users.noreply.github.com>

pkg/epp/framework/plugins/requestcontrol/dataproducer/inflightload/producer.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"encoding/json"
 	"reflect"
+	"sort"
 	"sync"
 	"sync/atomic"
 
@@ -54,6 +55,7 @@ var (
 	_ requestcontrol.DataProducer          = &InFlightLoadProducer{}
 	_ datalayer.EndpointExtractor          = &InFlightLoadProducer{}
 	_ datalayer.Registrant                 = &InFlightLoadProducer{}
+	_ fwkplugin.StateDumper                = &InFlightLoadProducer{}
 )
 
 type InFlightLoadProducer struct {
@@ -63,8 +65,57 @@ type InFlightLoadProducer struct {
 	tokenEstimator TokenEstimator
 }
 
+type InFlightLoadState struct {
+	Endpoints []EndpointInFlightLoadState `json:"endpoints"`
+}
+
+type EndpointInFlightLoadState struct {
+	Endpoint string `json:"endpoint"`
+	Requests int64  `json:"requests"`
+	Tokens   int64  `json:"tokens"`
+}
+
 func (p *InFlightLoadProducer) TypedName() fwkplugin.TypedName {
 	return p.typedName
+}
+
+func (p *InFlightLoadProducer) DumpState() any {
+	requestCounts := map[string]int64{}
+	if p.requestTracker != nil {
+		requestCounts = p.requestTracker.snapshot()
+	}
+
+	tokenCounts := map[string]int64{}
+	if p.tokenTracker != nil {
+		tokenCounts = p.tokenTracker.snapshot()
+	}
+
+	endpointSet := make(map[string]struct{}, len(requestCounts)+len(tokenCounts))
+	for endpointID := range requestCounts {
+		endpointSet[endpointID] = struct{}{}
+	}
+	for endpointID := range tokenCounts {
+		endpointSet[endpointID] = struct{}{}
+	}
+
+	endpointIDs := make([]string, 0, len(endpointSet))
+	for endpointID := range endpointSet {
+		endpointIDs = append(endpointIDs, endpointID)
+	}
+	sort.Strings(endpointIDs)
+
+	state := InFlightLoadState{
+		Endpoints: make([]EndpointInFlightLoadState, 0, len(endpointIDs)),
+	}
+	for _, endpointID := range endpointIDs {
+		state.Endpoints = append(state.Endpoints, EndpointInFlightLoadState{
+			Endpoint: endpointID,
+			Requests: requestCounts[endpointID],
+			Tokens:   tokenCounts[endpointID],
+		})
+	}
+
+	return state
 }
 
 // RegisterDependencies declares that this plugin needs an endpoint-notification-source to track
@@ -212,6 +263,17 @@ func (ct *concurrencyTracker) get(endpointID string) int64 {
 		return 0
 	}
 	return counter.Load()
+}
+
+func (ct *concurrencyTracker) snapshot() map[string]int64 {
+	ct.mu.RLock()
+	defer ct.mu.RUnlock()
+
+	result := make(map[string]int64, len(ct.counts))
+	for endpointID, counter := range ct.counts {
+		result[endpointID] = counter.Load()
+	}
+	return result
 }
 
 func (ct *concurrencyTracker) inc(endpointID string) {

llm-d/llm-d-router

pkg/epp/framework/plugins/requestcontrol/dataproducer/inflightload/producer.go
@@ -21,6 +21,8 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"reflect"
+	"sort"
 	"sync"
 	"sync/atomic"
 
@@ -41,6 +43,7 @@ import (
 const (
 	InFlightLoadProducerType = inflightloadconstants.InFlightLoadProducerType
 	profilePrefill           = "prefill"
+	maxDebugDumpEndpoints    = 100
 )
 
 // Config controls optional behaviors of InFlightLoadProducer.
@@ -95,6 +98,7 @@ var (
 	_ datalayer.EndpointExtractor          = (*InFlightLoadProducer)(nil)
 	_ datalayer.Registrant                 = &InFlightLoadProducer{}
 	_ fwkplugin.ConsumerPlugin             = &InFlightLoadProducer{}
+	_ fwkplugin.StateDumper                = &InFlightLoadProducer{}
 )
 
 type InFlightLoadProducer struct {
@@ -171,8 +175,92 @@ func (e *addedTokensEntry) OnEvicted(_ string, _ fwkplugin.StateKey) {
 	}
 }
 
+type inFlightLoadState struct {
+	Endpoints      []endpointInFlightLoadState `json:"endpoints"`
+	TotalEndpoints int                         `json:"totalEndpoints"`
+	MaxEndpoints   int                         `json:"maxEndpoints"`
+	Truncated      bool                        `json:"truncated"`
+}
+
+type endpointInFlightLoadState struct {
+	Endpoint string `json:"endpoint"`
+	Requests int64  `json:"requests"`
+	Tokens   int64  `json:"tokens"`
+}
+
 func (p *InFlightLoadProducer) TypedName() fwkplugin.TypedName {
 	return p.typedName
+}
+
+// DumpState implements [fwkplugin.StateDumper] and exposes per-endpoint
+// in-flight request and token counts for the /debug/plugins/state endpoint.
+//
+// The request and token tracker maps are snapshotted under separate read
+// locks, so the returned per-endpoint Requests and Tokens values are not
+// guaranteed to correspond to the same instant in time and the endpoint set
+// itself may change between the two snapshots. This is acceptable for a
+// debug endpoint, where best-effort visibility is preferred over coordinating
+// a single global lock that would contend with the hot path.
+//
+// The endpoint list is capped to the busiest endpoints to keep the debug
+// payload bounded when a deployment has a large endpoint set.
+func (p *InFlightLoadProducer) DumpState() (json.RawMessage, error) {
+	state := p.snapshotState()
+	return json.Marshal(state)
+}
+
+func (p *InFlightLoadProducer) snapshotState() inFlightLoadState {
+	requestCounts := map[string]int64{}
+	if p.requestTracker != nil {
+		requestCounts = p.requestTracker.snapshot()
+	}
+
+	tokenCounts := map[string]int64{}
+	if p.tokenTracker != nil {
+		tokenCounts = p.tokenTracker.snapshot()
+	}
+
+	endpointSet := make(map[string]struct{}, len(requestCounts)+len(tokenCounts))
+	for endpointID := range requestCounts {
+		endpointSet[endpointID] = struct{}{}
+	}
+	for endpointID := range tokenCounts {
+		endpointSet[endpointID] = struct{}{}
+	}
+
+	endpointIDs := make([]string, 0, len(endpointSet))
+	for endpointID := range endpointSet {
+		endpointIDs = append(endpointIDs, endpointID)
+	}
+	sort.Strings(endpointIDs)
+
+	state := inFlightLoadState{
+		Endpoints:      make([]endpointInFlightLoadState, 0, len(endpointIDs)),
+		TotalEndpoints: len(endpointIDs),
+		MaxEndpoints:   maxDebugDumpEndpoints,
+	}
+	for _, endpointID := range endpointIDs {
+		state.Endpoints = append(state.Endpoints, endpointInFlightLoadState{
+			Endpoint: endpointID,
+			Requests: requestCounts[endpointID],
+			Tokens:   tokenCounts[endpointID],
+		})
+	}
+
+	sort.SliceStable(state.Endpoints, func(i, j int) bool {
+		iLoad := state.Endpoints[i].Requests + state.Endpoints[i].Tokens
+		jLoad := state.Endpoints[j].Requests + state.Endpoints[j].Tokens
+		if iLoad != jLoad {
+			return iLoad > jLoad
+		}
+		return state.Endpoints[i].Endpoint < state.Endpoints[j].Endpoint
+	})
+	if len(state.Endpoints) > maxDebugDumpEndpoints {
+		state.Endpoints = state.Endpoints[:maxDebugDumpEndpoints]
+		state.Truncated = true
+	}
+
+	return state
 }
 
 // RegisterDependencies declares that this plugin needs an endpoint-notification-source to track
@@ -512,6 +600,17 @@ func (t *concurrencyTracker) get(endpointID string) int64 {
 		return 0
 	}
 	return counter.Load()
+}
+
+func (t *concurrencyTracker) snapshot() map[string]int64 {
+	t.mu.RLock()
+	defer t.mu.RUnlock()
+
+	result := make(map[string]int64, len(t.counts))
+	for endpointID, counter := range t.counts {
+		result[endpointID] = counter.Load()
+	}
+	return result
 }
 
 func (t *concurrencyTracker) inc(endpointID string) {