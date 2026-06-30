llm-d/llm-d-inference-scheduler

fix: address review feedback — rename metric and add decider tests
- Rename DeciderEvaluationCount to SchedulerDeciderEvaluationCount for
  consistency with existing SchedulerPDDecisionCount and
  SchedulerDisaggDecisionCount naming convention.
- Add TestDisaggregateMetricReasons to verify each disaggregate() return
  path records the correct reason metric label.

Ref #792

Signed-off-by: wenhug <50309350+wenhug@users.noreply.github.com>

pkg/plugins/profile/prefix_based_pd_decider_test.go
@@ -7,11 +7,13 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	k8stypes "k8s.io/apimachinery/pkg/types"
 	fwkdl "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/interface/datalayer"
 	"sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/interface/scheduling"
 	prefix "sigs.k8s.io/gateway-api-inference-extension/pkg/epp/framework/plugins/datalayer/attribute/prefix"
 
+	"github.com/llm-d/llm-d-inference-scheduler/pkg/metrics"
 	"github.com/llm-d/llm-d-inference-scheduler/test/utils"
 )
 
@@ -319,4 +321,76 @@ func TestWithName(t *testing.T) {
 
 	decider.WithName("renamed")
 	assert.Equal(t, "renamed", decider.TypedName().Name)
+}
+
+func TestDisaggregateMetricReasons(t *testing.T) {
+	ctx := utils.NewTestContext(t)
+
+	tests := []struct {
+		name           string
+		nonCachedToks  int
+		request        *scheduling.LLMRequest
+		endpoint       scheduling.Endpoint
+		expectedReason string
+	}{
+		{
+			name:           "disabled records disabled reason",
+			nonCachedToks:  0,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(5),
+			expectedReason: metrics.DeciderReasonDisabled,
+		},
+		{
+			name:           "nil endpoint records error reason",
+			nonCachedToks:  5,
+			request:        makeRequestWithTokens(10),
+			endpoint:       nil,
+			expectedReason: metrics.DeciderReasonError,
+		},
+		{
+			name:           "input too short records input_too_short reason",
+			nonCachedToks:  20,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(0),
+			expectedReason: metrics.DeciderReasonInputTooShort,
+		},
+		{
+			name:           "missing prefix info records error reason",
+			nonCachedToks:  5,
+			request:        makeRequestWithTokens(100),
+			endpoint:       makeTestEndpointBase(),
+			expectedReason: metrics.DeciderReasonError,
+		},
+		{
+			name:           "suffix cached records suffix_cached reason",
+			nonCachedToks:  5,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(8),
+			expectedReason: metrics.DeciderReasonSuffixCached,
+		},
+		{
+			name:           "disaggregated records disaggregated reason",
+			nonCachedToks:  3,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(2),
+			expectedReason: metrics.DeciderReasonDisaggregated,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics.SchedulerDeciderEvaluationCount.Reset()
+
+			decider, err := NewPrefixBasedPDDecider(PrefixBasedPDDeciderConfig{NonCachedTokens: tt.nonCachedToks})
+			require.NoError(t, err)
+
+			decider.disaggregate(ctx, tt.request, tt.endpoint)
+
+			count := testutil.ToFloat64(
+				metrics.SchedulerDeciderEvaluationCount.WithLabelValues("unknown", PrefixBasedPDDeciderPluginType, tt.expectedReason),
+			)
+			assert.Equal(t, float64(1), count,
+				"expected reason %q to be recorded once", tt.expectedReason)
+		})
+	}
 }

llm-d/llm-d-router

pkg/epp/framework/plugins/scheduling/profilehandler/disagg/prefix_based_pd_decider_test.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/prometheus/client_golang/prometheus/testutil"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	k8stypes "k8s.io/apimachinery/pkg/types"
@@ -14,6 +15,7 @@ import (
 	fwkrh "github.com/llm-d/llm-d-router/pkg/epp/framework/interface/requesthandling"
 	"github.com/llm-d/llm-d-router/pkg/epp/framework/interface/scheduling"
 	attrprefix "github.com/llm-d/llm-d-router/pkg/epp/framework/plugins/datalayer/attribute/prefix"
+	"github.com/llm-d/llm-d-router/pkg/metrics"
 	"github.com/llm-d/llm-d-router/test/utils"
 )
 
@@ -466,4 +468,77 @@ func TestWithName(t *testing.T) {
 
 	decider.WithName("renamed")
 	assert.Equal(t, "renamed", decider.TypedName().Name)
+}
+
+func TestDisaggregateMetricReasons(t *testing.T) {
+	ctx := utils.NewTestContext(t)
+
+	tests := []struct {
+		name           string
+		nonCachedToks  int
+		request        *scheduling.InferenceRequest
+		endpoint       scheduling.Endpoint
+		expectedReason string
+	}{
+		{
+			name:           "disabled records disabled reason",
+			nonCachedToks:  0,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(5),
+			expectedReason: metrics.DeciderReasonDisabled,
+		},
+		{
+			name:           "nil endpoint records error reason",
+			nonCachedToks:  5,
+			request:        makeRequestWithTokens(10),
+			endpoint:       nil,
+			expectedReason: metrics.DeciderReasonError,
+		},
+		{
+			name:           "input too short records input_too_short reason",
+			nonCachedToks:  20,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(0),
+			expectedReason: metrics.DeciderReasonInputTooShort,
+		},
+		{
+			name:           "missing prefix info records error reason",
+			nonCachedToks:  5,
+			request:        makeRequestWithTokens(100),
+			endpoint:       makeTestEndpointBase(),
+			expectedReason: metrics.DeciderReasonError,
+		},
+		{
+			name:           "suffix cached records suffix_cached reason",
+			nonCachedToks:  5,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(8),
+			expectedReason: metrics.DeciderReasonSuffixCached,
+		},
+		{
+			name:           "disaggregated records disaggregated reason",
+			nonCachedToks:  3,
+			request:        makeRequestWithTokens(10),
+			endpoint:       makeTestEndpoint(2),
+			expectedReason: metrics.DeciderReasonDisaggregated,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			metrics.SchedulerDeciderEvaluationCount.Reset()
+			metrics.LlmdDeciderEvaluationCount.Reset()
+
+			decider, err := NewPrefixBasedPDDecider(PrefixBasedPDDeciderConfig{NonCachedTokens: tt.nonCachedToks})
+			require.NoError(t, err)
+
+			decider.disaggregate(ctx, tt.request, tt.endpoint)
+
+			count := testutil.ToFloat64(
+				metrics.SchedulerDeciderEvaluationCount.WithLabelValues("unknown", PrefixBasedPDDeciderPluginType, tt.expectedReason),
+			)
+			assert.Equal(t, float64(1), count,
+				"expected reason %q to be recorded once", tt.expectedReason)
+		})
+	}
 }