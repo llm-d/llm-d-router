d46f4b8321bf1d61798ba4a8ff9515a73d6f7dc8
Loic Marchal <lmarchal@redhat.com>
2026-07-29 00:08:35 -0400

    rename "global" metrics by "effective"
    Signed-off-by: Loic Marchal <lmarchal@redhat.com>

--- pkg/epp/flowcontrol/controller/internal/processor.go
+++ pkg/epp/flowcontrol/controller/internal/processor.go
@@ -471,7 +471,7 @@ func (p *Processor) dispatchCycle(ctx context.Context) bool {
 		saturation = p.saturationDetector.Saturation(ctx, pool)
 	}
 
-	metrics.RecordFlowControlPoolSaturation(p.poolName, "global", saturation)
+	metrics.RecordFlowControlPoolSaturation(p.poolName, "effective", saturation)
 
 	// Record capacity utilization ratios (the demand-side twin of saturation) from the same periodic sample.
 	p.recordCapacityUtilization()
